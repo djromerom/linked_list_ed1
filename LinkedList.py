0c3d8aaa82475c300b39dee81e2c226a6b19871d
Daniel Romero <djromer@gmail.com>
2026-09-23 16:57:41 -0500

    insert_at_begging_with_tail

--- LinkedList.py
+++ LinkedList.py
@@ -7,11 +7,21 @@ class LinkedList:
     """
     def __init__(self):
         self.head = None
+        self.tail = None
 
     def insert_at_beginning(self, data):
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
+
+    def insert_at_begging_with_tail(self, data):
+            new_node = Node(data)
+            if self.head is None:
+                self.head = new_node
+                self.tail = new_node
+                return
+            self.tail.next = new_node
+            self.tail = new_node
 
     def insert_at_end(self, data):
         new_node = Node(data)
--- Main.py
+++ Main.py
@@ -2,9 +2,10 @@ from LinkedList import LinkedList
 
 ll = LinkedList()
 
-ll.insert_at_beginning(20)
-ll.insert_at_beginning(10)
-ll.insert_at_end(30)
-ll.insert_at_end(40)
+ll.insert_at_begging_with_tail(20)
+ll.insert_at_begging_with_tail(20)
+ll.insert_at_begging_with_tail(10)
+ll.insert_at_begging_with_tail(30)
+ll.insert_at_begging_with_tail(40)
 
 ll.display()
