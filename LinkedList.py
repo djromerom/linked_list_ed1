4f83514ad24f05ce9ac9554142c67cacd04160cd
Daniel Romero <djromer@gmail.com>
2026-09-23 15:36:12 -0500

    primer commit

---
+++ LinkedList.py
@@ -0,0 +1,42 @@
+from Node import Node
+
+class LinkedList:
+    """
+    Empty Constructor
+    
+    """
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = new_node
+
+    
+    def search(self, target):
+        current = self.head
+
+        while current is not None:
+            if current.data == target:
+                return True
+            current = current.next
+        return False
+    
+    def display(self):
+            current = self.head
+            while current is not None:
+                print(current.data, end=" -> ")
+                current = current.next
+            print("None")
---
+++ Main.py
@@ -0,0 +1,10 @@
+from LinkedList import LinkedList
+
+ll = LinkedList()
+
+ll.insert_at_beginning(20)
+ll.insert_at_beginning(10)
+ll.insert_at_end(30)
+ll.insert_at_end(40)
+
+ll.display()
